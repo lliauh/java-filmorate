4b9135dd771d565da6739b0c07929766c74b0e0c
lliauh <lliauh@gmail.com>
2024-02-04 20:29:33 +0100

    fix: исправлена опечатка

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -7,6 +7,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FilmorateApplication {
 	public static void main (String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
-	}
-
 }
